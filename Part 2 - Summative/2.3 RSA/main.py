72248f293536212eb2656d59cc239770be2b2f5f
ezra <ezra.mcbee@gmail.com>
2024-09-16 11:07:00 -0400

    day 7

--- Part 2 - Summative/2.3 RSA/main.py
+++ Part 2 - Summative/2.3 RSA/main.py
@@ -3,8 +3,19 @@ import math
 # Copy and paste any functions you need from the Affine assignment!
 
 # Also write these:
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 def rsa_encode(text, m, e):
-  return 0
+  num = 0
+  for i in range(len(text)):
+    if text[i].upper() in alpha:
+      x = alpha.find(text[i].upper())
+      y = 26 ** i
+      num += x * y
+  if num < m:
+    return pow(num,e,m)
+  else:
+    return "String too long!"
+
 
 def rsa_decode(num, m, d, l):
   return ""
@@ -12,6 +23,9 @@ def rsa_decode(num, m, d, l):
 def get_d(p, q, e):
   return pow()
 
+p = 292361466231755597564095925310764764819
+q = 307125506157764866722739041634199200019
+print(rsa_encode("THEFIVEBOXINGWIZARDSJUMPQUICKLY",p*q,65537))
 # text = "THEFIVEBOXINGWIZARDSJUMPQUICKLY"
 # l = len(text)
 # p = 292361466231755597564095925310764764819
@@ -28,14 +42,14 @@ def get_d(p, q, e):
 
 # Part 2: Generate your own key!
 
-from sympy import nextprime
-from random import randint
-
-def make_prime(n):
-  lower = 2 ** (n - 1) + 1
-  upper = 2 ** n - 1
-  temp = randint(lower, upper)
-  return nextprime(temp)
+# from sympy import nextprime
+# from random import randint
+#
+# def make_prime(n):
+#   lower = 2 ** (n - 1) + 1
+#   upper = 2 ** n - 1
+#   temp = randint(lower, upper)
+#   return nextprime(temp)
 
 
 
