644a72b391631f9dd0a495f2711bd7d247f85605
ezra <ezra.mcbee@gmail.com>
2024-09-17 10:41:05 -0400

    Completed "Coding RSA" Segment

--- Part 2 - Summative/2.3 RSA/main.py
+++ Part 2 - Summative/2.3 RSA/main.py
@@ -4,6 +4,21 @@ import math
 
 # Also write these:
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def mod_inverse_helper(a, b):
+  q, r = a // b, a % b
+  if r == 1:
+    return (1, -1 * q)
+  u, v = mod_inverse_helper(b, r)
+  return (v, -1 * q * v + u)
+
+
+def mod_inverse(a, m):
+  assert math.gcd(a, m) == 1, "You're trying to invert " + str(a) + " in mod " + str(m) + " and that doesn't work!"
+  return mod_inverse_helper(m, a)[1] % m
+
+
 def rsa_encode(text, m, e):
   num = 0
   for i in range(len(text)):
@@ -18,26 +33,32 @@ def rsa_encode(text, m, e):
 
 
 def rsa_decode(num, m, d, l):
-  return ""
+  new_text = ""
+  x = pow(num, d, m)
+  for i in range(l):
+    new_text += alpha[x % 26]
+    x = x // 26
+  return new_text
 
 def get_d(p, q, e):
-  return pow()
+  t =  (p-1) * (q-1)
+  return mod_inverse(e, t)
 
+print(get_d(2003, 2503, 17))
+
+
+text = "THEFIVEBOXINGWIZARDSJUMPQUICKLY"
+l = len(text)
 p = 292361466231755597564095925310764764819
 q = 307125506157764866722739041634199200019
-print(rsa_encode("THEFIVEBOXINGWIZARDSJUMPQUICKLY",p*q,65537))
-# text = "THEFIVEBOXINGWIZARDSJUMPQUICKLY"
-# l = len(text)
-# p = 292361466231755597564095925310764764819
-# q = 307125506157764866722739041634199200019
-# e = 65537
-# m = p * q
-# d = get_d(p, q, e)
-# enc = rsa_encode(text, m, e)
-# dec = rsa_decode(enc, m, d, l)
-# print(enc)
-# print(dec)
-# If this works, dec should be the same as text!
+e = 65537
+m = p * q
+d = get_d(p, q, e)
+enc = rsa_encode(text, m, e)
+dec = rsa_decode(enc, m, d, l)
+print(enc)
+print(dec)
+#If this works, dec should be the same as text!
 
 
 # Part 2: Generate your own key!
