155114919d6464a97c8b2a83be1accbfa7e18db2
ezra <ezra.mcbee@gmail.com>
2024-09-17 10:56:01 -0400

    finished RSA code, start unit testing

--- Part 2 - Summative/2.3 RSA/main.py
+++ Part 2 - Summative/2.3 RSA/main.py
@@ -1,5 +1,6 @@
 import math
-
+from sympy import nextprime
+from random import randint
 # Copy and paste any functions you need from the Affine assignment!
 
 # Also write these:
@@ -62,56 +63,21 @@ print(dec)
 
 
 # Part 2: Generate your own key!
-
-# from sympy import nextprime
-# from random import randint
-#
-# def make_prime(n):
-#   lower = 2 ** (n - 1) + 1
-#   upper = 2 ** n - 1
-#   temp = randint(lower, upper)
-#   return nextprime(temp)
-
-
-
-
-'''import math
-
-# Copy and paste any functions you need from the Affine assignment!
-
-# Also write these:
-def rsa_encode(text, m, e):
-  return 0
-
-def rsa_decode(num, m, d, l):
-  return ""
-
-def get_d(p, q, e):
-  return 0
-
-text = "THEFIVEBOXINGWIZARDSJUMPQUICKLY"
-l = len(text)
-p = 292361466231755597564095925310764764819
-q = 307125506157764866722739041634199200019
-e = 65537
-m = p * q
-d = get_d(p, q, e)
-enc = rsa_encode(text, m, e)
-dec = rsa_decode(enc, m, d, l)
-print(enc)
-print(dec)
-# If this works, dec should be the same as text!
-
-
-# Part 2: Generate your own key!
-
-from sympy import nextprime
-from random import randint
-
 def make_prime(n):
   lower = 2 ** (n - 1) + 1
   upper = 2 ** n - 1
   temp = randint(lower, upper)
   return nextprime(temp)
 
-'''
+
+p = make_prime(100)
+q = make_prime(100)
+e = 65537
+m = p * q
+d = get_d(p, q, e)
+message = "FUNNEWMESSAGE"
+l = len(message)
+encoded = rsa_encode(message, m, e)
+
+print(encoded)
+print(rsa_decode(encoded, m, d, l))
